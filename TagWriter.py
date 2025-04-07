3abfaf7f2ed3eb8c8f0e1daec8fc04e74340a6b3
priortecsolucoes <priortecsolucoes@gmail.com>
2025-04-07 10:33:58 -0300

    Update TagWriter.py

--- TagWriter.py
+++ TagWriter.py
@@ -1,46 +1,87 @@
-from fastapi import FastAPI
-import psycopg2
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import psycopg2
+import os
 
 app = FastAPI()
 
+VALID_ACCESS_KEY = os.getenv("EXPECTED_ACCESS_KEY") 
 
 class WriteTagRequest(BaseModel):
-    tag_name: str
-    string_value: str
-    int_value: int
-    double_value: float
+    tagName: str
+    stringValue: str
+    intValue: int
+    doubleValue: float
+    accessKey: int  
 
+class TagService:
+    @staticmethod
+    def getDbConnection():
+        return psycopg2.connect(
+            host=os.getenv('HOST'),
+            database=os.getenv('DATABASE'),
+            user=os.getenv('USER'),
+            password=os.getenv('PASSWORD'),
+            port=os.getenv('PORT')
+        )
+    @staticmethod
+    def validateAccessKey(accessKey):
+        if accessKey != VALID_ACCESS_KEY:
+            raise HTTPException(status_code=403, detail="Access denied: Invalid access key")
 
-def get_db_connection():
-    return psycopg2.connect(
-        host="roundhouse.proxy.rlwy.net",
-        database="railway",
-        user="postgres",
-        password="XWhTqAztzzYuAqvUcgWCUJUJmnEJliDK",
-        port=28938
-    )
+    @staticmethod
+    def updateTag(request: WriteTagRequest):
+        TagService.validateAccessKey(request.accessKey) # Valida a accessKey antes de qualquer operacao
+        try:
+            conn = TagService.getDbConnection()
+            cursor = conn.cursor()
+            
+            # Verifica se a tag existe
+            selectTagQuery = "SELECT id FROM tag WHERE name = %s"
+            cursor.execute(selectTagQuery, (request.tagName,))
+            tagId = cursor.fetchone()
+            if tagId:
+                tagId = tagId[0]
+
+                # Verifica se ja existem valores para a tag
+                checkValueQuery = "SELECT * FROM tag_value WHERE tag_id = %s"
+                cursor.execute(checkValueQuery, (tagId,))
+                existingValue = cursor.fetchone()
+
+                if existingValue:
+                    updateQuery = """
+                    UPDATE tag_value
+                    SET string_value = %s, int_value = %s, double_value = %s
+                    WHERE tag_id = %s
+                    """
+                    cursor.execute(updateQuery, (
+                        request.stringValue, 
+                        request.intValue, 
+                        request.doubleValue, 
+                        tagId
+                    ))
+                else:# Insere o primeiro valor caso nao exista
+                    insertValueQuery = """
+                    INSERT INTO tag_value (tag_id, string_value, int_value, double_value)
+                    VALUES (%s, %s, %s, %s)
+                    """
+                    cursor.execute(insertValueQuery, (
+                        tagId, 
+                        request.stringValue, 
+                        request.intValue, 
+                        request.doubleValue
+                    ))
+                conn.commit()
+                message = "Tag atualizada com sucesso"
+            else:
+                message = "Tag não encontrada. Nenhuma ação foi realizada."
+            cursor.close()
+            conn.close()
+            return {"message": message}
+        except Exception as e:
+            return {"message": f"Erro ao tentar realizar escrita de tag: {e}"}
 
 
 @app.put("/update-tag")
-def write_tag(request: WriteTagRequest):
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-    
-        update_query = """
-        UPDATE tag_value
-        SET string_value = %s, int_value = %s, double_value = %s
-        WHERE tag_id = (
-            SELECT id FROM tag WHERE name = %s
-        ) 
-        """
-    
-        cursor.execute(update_query, (request.string_value,
-                       request.int_value, request.double_value, request.tag_name))
-        conn.commit()
-        cursor.close()
-        conn.close()
-        return {"message": "Tag atualizada com sucesso"}
-    except Exception as e:
-        return {"message": f"Erro ao tentar realizar escrita de tag: {e}" }
+def writeTag(request: WriteTagRequest):
+    return TagService.updateTag(request)
